efd71b0224a5d7108beee444ef8b5cd127a9be23
SMNoushan <noushan.md@gmail.com>
2024-07-06 22:27:38 +0600

    highlighted category css

--- src/components/Categories/CategoryBox.jsx
+++ src/components/Categories/CategoryBox.jsx
@@ -1,9 +1,12 @@
 import PropTypes from "prop-types";
 import queryString from "query-string";
-import { useNavigate } from "react-router-dom";
+import { useNavigate, useSearchParams } from "react-router-dom";
 
 const CategoryBox = ({ label, icon: Icon }) => {
   const navigate = useNavigate();
+  const [params] = useSearchParams();
+  const category = params.get("category");
+
   const handleClick = () => {
     const currentQuery = {
       category: label,
@@ -14,16 +17,11 @@ const CategoryBox = ({ label, icon: Icon }) => {
   return (
     <div
       onClick={handleClick}
-      className={`flex 
-  flex-col 
-  items-center 
-  justify-center 
-  gap-2
-  p-3
-  border-b-2
-  hover:text-neutral-800
-  transition
-  cursor-pointer`}
+      className={`flex flex-col items-center justify-center gap-2 p-3 border-b-2 hover:text-neutral-800 transition cursor-pointer 
+      ${
+        category?.toLocaleLowerCase() === label.toLocaleLowerCase() &&
+        "border-b-neutral-800 text-neutral-800"
+      }`}
     >
       <Icon size={26} />
       <div className="text-sm font-medium">{label}</div>

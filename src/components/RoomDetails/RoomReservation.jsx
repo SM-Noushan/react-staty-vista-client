6fedf70b9e0f2b89271def6b2f4523e2012ac9b0
SMNoushan <noushan.md@gmail.com>
2024-07-08 12:58:55 +0600

    fixed totalday/totalnight counts

--- src/components/RoomDetails/RoomReservation.jsx
+++ src/components/RoomDetails/RoomReservation.jsx
@@ -1,31 +1,45 @@
 import { useState } from "react";
 import PropTypes from "prop-types";
 import { DateRange } from "react-date-range";
+import { differenceInCalendarDays } from "date-fns";
 
 import Button from "../Shared/Button/Button";
 
 const RoomReservation = ({ room }) => {
   const [state, setState] = useState([
     {
-      startDate: new Date(),
-      endDate: null,
+      startDate: new Date(room?.from),
+      endDate: new Date(room?.to),
       key: "selection",
     },
   ]);
+  const totalDays = parseInt(
+    differenceInCalendarDays(new Date(room?.to), new Date(room?.from))
+  );
   return (
     <div className="rounded-xl border-[1px] border-neutral-200 overflow-hidden bg-white">
       <div className="flex items-center gap-1 p-4">
         <div className="text-2xl font-semibold">$ {room?.price}</div>
-        <div className="font-light text-neutral-600">night</div>
+        <div className="font-light text-neutral-600">/night</div>
       </div>
       <hr />
       <div className="flex justify-center">
         <DateRange
           rangeColors={["#F6536D"]}
-          editableDateInputs={true}
-          onChange={(item) => setState([item.selection])}
-          moveRangeOnFirstSelection={false}
+          // editableDateInputs={true}
+          onChange={() => {
+            setState([
+              {
+                startDate: new Date(room?.from),
+                endDate: new Date(room?.to),
+                key: "selection",
+              },
+            ]);
+          }}
+          moveRangeOnFirstSelection={true}
           ranges={state}
+          minDate={new Date(room?.from)}
+          maxDate={new Date(room?.to)}
           showDateDisplay={false}
         />
       </div>
@@ -36,7 +50,7 @@ const RoomReservation = ({ room }) => {
       <hr />
       <div className="p-4 flex items-center justify-between font-semibold text-lg">
         <div>Total</div>
-        <div>${room?.price}</div>
+        <div>${room?.price * totalDays}</div>
       </div>
     </div>
   );

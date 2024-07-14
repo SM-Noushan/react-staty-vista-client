124969844511ff0afd333ed0faa2ca526c0839b7
SMNoushan <noushan.md@gmail.com>
2024-07-14 22:33:18 +0600

    added: host stats and guest stats

--- src/pages/Dashboard/Common/Statistics.jsx
+++ src/pages/Dashboard/Common/Statistics.jsx
@@ -1,13 +1,25 @@
-import AdminStatistics from "../../../components/Dashboard/Statistics/AdminStatistics";
-import LoadingSpinner from "../../../components/Shared/LoadingSpinner";
 import useRole from "../../../hooks/useRole";
+import LoadingSpinner from "../../../components/Shared/LoadingSpinner";
+import HostStatistics from "../../../components/Dashboard/Statistics/HostStatistics";
+import AdminStatistics from "../../../components/Dashboard/Statistics/AdminStatistics";
+import GuestStatistics from "../../../components/Dashboard/Statistics/GuestStatistics";
 
 const Statistics = () => {
   const [role, isLoading] = useRole();
 
   if (isLoading) return <LoadingSpinner />;
 
-  return <>{role === "admin" && <AdminStatistics />}</>;
+  return (
+    <>
+      {role === "admin" ? (
+        <AdminStatistics />
+      ) : role === "host" ? (
+        <HostStatistics />
+      ) : (
+        <GuestStatistics />
+      )}
+    </>
+  );
 };
 
 export default Statistics;
